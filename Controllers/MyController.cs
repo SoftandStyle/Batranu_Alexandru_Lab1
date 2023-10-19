135d965e51f34030babf336ac0c9ec93264253dd
SoftandStyle <alexandrubatranu28@gmail.com>
2023-10-19 18:11:21 +0300

    Prima pagina modificata.

---
+++ Controllers/MyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Batranu_Alexandru_Lab1.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
