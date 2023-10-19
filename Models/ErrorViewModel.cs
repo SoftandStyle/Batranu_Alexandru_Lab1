44faaa12b7df1caf86f9203f39fb89010f1fcf9f
alexu <alexu@DESKTOP-TTLGJF2>
2023-10-19 16:04:48 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Batranu_Alexandru_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
